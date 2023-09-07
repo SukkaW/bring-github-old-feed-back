8104f46a0f9d029e49ebdb19e54e2cf7e231271d
SukkaW <isukkaw@gmail.com>
2023-09-07 15:17:26 +0800

    chore: build *.meta.js for updateURL

--- rollup.config.ts
+++ rollup.config.ts
@@ -12,24 +12,35 @@ const userScriptMetaBlockConfig = {
   }
 };
 
-export default defineConfig({
-  input: 'src/index.ts',
-  output: [{
-    format: 'iife',
-    file: 'dist/bring-github-old-feed-back.user.js',
-    sourcemap: false,
-    esModule: false,
-    compact: true,
-    generatedCode: 'es2015'
-  }],
-  plugins: [
-    swc(defineRollupSwcOption({
-      jsc: {
-        target: 'es2020',
-        externalHelpers: true
-      }
-    })),
-    metablock(userScriptMetaBlockConfig)
-  ],
-  external: ['typed-query-selector']
-});
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: [{
+      format: 'iife',
+      file: 'dist/bring-github-old-feed-back.user.js',
+      sourcemap: false,
+      esModule: false,
+      compact: true,
+      generatedCode: 'es2015'
+    }],
+    plugins: [
+      swc(defineRollupSwcOption({
+        jsc: {
+          target: 'es2020',
+          externalHelpers: true
+        }
+      })),
+      metablock(userScriptMetaBlockConfig)
+    ],
+    external: ['typed-query-selector']
+  },
+  {
+    input: 'src/dummy.js',
+    output: [{
+      file: 'dist/bring-github-old-feed-back.meta.js'
+    }],
+    plugins: [
+      metablock(userScriptMetaBlockConfig)
+    ]
+  }
+]);
