f39fe77708db2482121ccd1cdaa616ba27f4832a
SukkaW <isukkaw@gmail.com>
2023-09-07 14:17:42 +0800

    feat: initial implementation

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import 'typed-query-selector';
+
+const fetchDashboardFeedHtml = async () => {
+  const res = await fetch('https://github.com/dashboard-feed', {
+    headers: {
+      // With this header, GitHub will not return App Shell, only the content's HTML
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  });
+  return res.text();
+};
+
+const loading = document.createElement('p');
+loading.textContent = 'Loading...';
+
+(async () => {
+  const targetEl = document.querySelector('#dashboard > div > feed-container > div[data-target="feed-container.content"]');
+  if (targetEl) {
+    // refine-github's infinite-scroll feature is targetting `[role="tabpanel"]:not([hidden]) button.ajax-pagination-btn`
+    // Add the missing `role="tabpanel"` to the target element to make it work
+    targetEl.role = 'tabpanel';
+
+    const oldDashboard = document.createElement('template');
+    oldDashboard.append(loading);
+    targetEl.innerHTML = oldDashboard.innerHTML;
+    // Wait for the new dashboard to be loaded
+    targetEl.innerHTML = await fetchDashboardFeedHtml();
+  }
+})();
